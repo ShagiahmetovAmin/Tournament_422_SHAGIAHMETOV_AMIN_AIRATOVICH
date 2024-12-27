c0f5fbcb1e7bfe8dce021c4d926b6ba8902f3547
ShagiahmetovAmin <shagiahmetovamin2759@gmail.com>
2024-12-27 17:25:47 +0300

    Добавил Страницу организатора

---
+++ Tournament_422_SHAGIAHMETOV_AMIN_AIRATOVICH/Pages/OrganizatorPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tournament_422_SHAGIAHMETOV_AMIN_AIRATOVICH.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для OrganizatorPage.xaml
+    /// </summary>
+    public partial class OrganizatorPage : Page
+    {
+        public OrganizatorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(typeof)
+        }
+    }
+}
